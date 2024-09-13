028cd839802c3ae17724577d885b33a11c974ab0
Mohamad Ikhsan Nurulloh <32507742+ikhsannur1996@users.noreply.github.com>
2024-09-13 09:49:35 +0700

    Update store_procedure.sql

--- store_procedure.sql
+++ store_procedure.sql
@@ -8,33 +8,65 @@ BEGIN
     TRUNCATE TABLE stg.stg_sales_transaction;
     INSERT INTO stg.stg_sales_transaction 
     SELECT *, CURRENT_TIMESTAMP AS last_update FROM public.sales_transaction;
- 
+
     -- Step 2: Insert or update into dim_product
     -- Description: Insert new product data into the product dimension if they don't already exist.
     --              Update existing product data if they already exist.
-    -- Answer: 
-    
+
     -- Step 3: Insert or update into dim_store
     -- Description: Insert new store data into the store dimension if they don't already exist.
     --              Update existing store data if they already exist.
-    -- Answer: 
 
-    
+
     -- Step 4: Insert or update into dim_time
     -- Description: Insert new time data into the time dimension if they don't already exist.
     --              Update existing time data if they already exist.
-    -- Answer: 
 
-    
+
     -- Step 5: Insert or update into dim_sales_name
     -- Description: Insert new sales name data into the sales name dimension if they don't already exist.
     --              Update existing sales name data if they already exist.
-    -- Answer: 
 
-    
+
     -- Step 6: Insert into fact_sales
     -- Description: Insert new sales data into the fact table if they don't already exist.
-   -- Answer: 
+    CREATE TABLE IF NOT EXISTS dwh.fact_sales (
+        sale_id INT,
+        store_id INT,
+        sales_name_id INT,
+        time_id INT,
+        product_id INT,
+        quantity INT,
+        price INT,
+        last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+    INSERT INTO dwh.fact_sales
+        (sale_id, store_id, sales_name_id, time_id, product_id, quantity, price, last_update)
+    SELECT
+        src.sale_id,
+        src.store_id,
+        src.sales_name_id,
+        src.time_id,
+        src.product_id,
+        src.quantity,
+        src.price,
+        CURRENT_TIMESTAMP AS last_update
+    FROM
+        stg.stg_sales_transaction AS src 
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM dwh.fact_sales AS fact
+        WHERE 
+            COALESCE(fact.sale_id, 0) = COALESCE(src.sale_id, 0) AND
+            COALESCE(fact.store_id, 0) = COALESCE(src.store_id, 0) AND
+            COALESCE(fact.sales_name_id, 0) = COALESCE(src.sales_name_id, 0) AND
+            COALESCE(fact.time_id, 0) = COALESCE(src.time_id, 0) AND
+            COALESCE(fact.product_id, 0) = COALESCE(src.product_id, 0) AND
+            COALESCE(fact.quantity, 0) = COALESCE(src.quantity, 0) AND
+            COALESCE(fact.price, 0) = COALESCE(src.price, 0)
+    );
+
 
     -- Step 7: Truncate and Insert into dm_sales_transaction
     -- Description: Populate the data mart with the latest sales transactions.
@@ -70,7 +102,7 @@ BEGIN
         dwh.dim_time dt ON fs.time_id = dt.time_id
     LEFT JOIN 
         dwh.dim_product dp ON fs.product_id = dp.product_id;
-    
+
     CREATE TABLE IF NOT EXISTS dm.dm_sales_transaction AS
     SELECT * 
     FROM (
@@ -79,7 +111,7 @@ BEGIN
         FROM dm.vw_dm_sales_transaction
     ) AS ranked_data
     WHERE flag_unique = 1;
-    
+
     TRUNCATE TABLE dm.dm_sales_transaction;
 
     INSERT INTO dm.dm_sales_transaction 
